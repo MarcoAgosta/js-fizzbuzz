f1fad5c810970775d5fab03adebb3819d15011e4
MarcoAgosta <agostamarco@libero.it>
2022-11-02 16:28:00 +0100

    Creati i colori differenti

--- JS/main.js
+++ JS/main.js
@@ -8,24 +8,24 @@ for (let i=1; i<=100; i++){
 
         console.log("Fizz")
 
-        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span> Fizz </span></div></div>`
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato giallo"><span> Fizz </span></div></div>`
 
     } else if (i % 5 == 0 && i % 3 !== 0){
 
         console.log("Buzz")
 
-        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span> Buzz </span></div></div>`
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato verde"><span> Buzz </span></div></div>`
     
     } else if (i % 3 == 0 && i % 5 == 0){
 
         console.log("FizzBuzz")
 
-        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span> FizzBuzz </span></div></div>`
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato rosso"><span> FizzBuzz </span></div></div>`
     
     } else{
 
         console.log(i)
 
-        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span>${i}</span></div></div>`
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato blu"><span>${i}</span></div></div>`
     }
 }
