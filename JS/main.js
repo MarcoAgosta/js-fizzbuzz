fe7a2d07a6f48a2d96f82c9ebdf58d3335468364
MarcoAgosta <agostamarco@libero.it>
2022-11-02 16:05:30 +0100

    Creata griglia coi cubi numerati

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,31 @@
+const contenitoreCubi = document.getElementById("contenitore")
+
+
+
 for (let i=1; i<=100; i++){
 
     if (i % 3 == 0 && i % 5 !== 0){
 
         console.log("Fizz")
 
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span> Fizz </span></div></div>`
+
     } else if (i % 5 == 0 && i % 3 !== 0){
 
         console.log("Buzz")
+
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span> Buzz </span></div></div>`
     
     } else if (i % 3 == 0 && i % 5 == 0){
 
         console.log("FizzBuzz")
+
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span> FizzBuzz </span></div></div>`
     
     } else{
 
         console.log(i)
 
+        contenitoreCubi.innerHTML += `<div class="col-1"><div class="quadrato"><span>${i}</span></div></div>`
     }
 }
